f788f8e63806b2e9585aa1b1ebe04bfea0836f68
Michal <michal.szeniak@gmail.com>
2024-08-12 01:48:34 +0200

    metatags - refactor

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import { Metadata } from "next";
 import Favicon from "/public/favicon.ico";
+import Og from "/public/og.png";
 
 export const metadata: Metadata = {
   title: "Michal Szeniak - portfolio",
@@ -10,17 +11,16 @@ export const metadata: Metadata = {
     description: "Check out my best works and projects!",
     url: "https://www.michalszeniak.pl/",
     type: "website",
-    images: ["/og.png"],
+    images: [Og.src],
   },
   twitter: {
     card: "summary_large_image",
     title: "My Twitter portfolio",
     description: "Check out my best works and projects!",
-    images: "/og.png",
-    creator: "@moj_twitter",
-    site: "@mojastrona",
+    images: [Og.src],
   },
-  keywords: "portfolio, web development, frontend, JavaScript, React",
+  keywords:
+    "michal szeniak, portfolio, web development, frontend, JavaScript, React",
   robots: "index,follow",
   authors: {
     name: "Michał Szeniak",
